072c28e4156a761f96c05778d2323c0862976344
oriolagobat <oriolagobat@gmail.com>
2022-04-11 13:28:07 +0200

    Implemented producer

--- kafka_producer/src/main/java/producertutorial/Producer.java
+++ kafka_producer/src/main/java/producertutorial/Producer.java
@@ -1,13 +1,25 @@
 package producertutorial;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 
 public class Producer {
+    public static KafkaProducer<String, String> producer;
+
     public static void main(String[] args) {
         Properties props = getProducerProperties();
+        producer = new KafkaProducer<>(props);
+
+        ProducerRecord<String, String> record = new ProducerRecord<>("my-topic", "Hello World");
+        producer.send(record);
+
+        sendMessageFromString("Second message");
+
+        closeProducer(producer);
     }
 
     private static Properties getProducerProperties() {
@@ -18,5 +30,15 @@ public class Producer {
         props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         return props;
+    }
+
+    private static void sendMessageFromString(String input) {
+        ProducerRecord<String, String> record = new ProducerRecord<>("my-topic", input);
+        producer.send(record);
+    }
+
+    private static void closeProducer(KafkaProducer<String, String> producer) {
+        producer.flush();
+        producer.close();
     }
 }
